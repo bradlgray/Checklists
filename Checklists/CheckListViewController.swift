247fef2ffbf255f67e749c6a56d6632c89cec885
Bradford Gray <bgray@westmont.edu>
2015-05-25 20:17:31 -0700

    Add  new class, create checklist check, uncheck

--- Checklists/CheckListViewController.swift
+++ Checklists/CheckListViewController.swift
@@ -9,6 +9,42 @@
 import UIKit
 
 class ChecklistViewController: UITableViewController {
+    
+    var row0text = "Walk the dog"
+    var row1text = "Brush teeth"
+    var row2text = "Learn iOS development"
+    var row3text = "Soccer practice"
+    var row4text = "Eat ice cream"
+    var row0checked = false
+    var row1checked = true
+    var row2checked = true
+    var row3checked = false
+    var row4checked = true
+    
+    func configureCheckmarkForCell(cell: UITableViewCell,
+        indexPath: NSIndexPath) {
+        var isChecked = false
+        if indexPath.row == 0 {
+        isChecked = row0checked
+    } else if indexPath.row == 1 {
+        isChecked = row1checked
+    } else if indexPath.row == 2 {
+        isChecked = row2checked
+    } else if indexPath.row == 3 {
+        isChecked = row3checked
+    } else if indexPath.row == 4 {
+        isChecked = row4checked
+        }
+        if isChecked {
+            cell.accessoryType = .Checkmark
+        } else {
+            cell.accessoryType = .None
+        }
+    }
+
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,35 +55,83 @@ class ChecklistViewController: UITableViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+   
+    
+override func tableView(tableview: UITableView,
+    numberOfRowsInSection: Int) ->Int {
+        return 5
+}
     override func tableView(tableView: UITableView,
-        numberOfRowsInSection section: Int) -> Int {
-        return 100
-    }
-    override func tableView(tableView: UITableView,
-        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-            
+        cellForRowAtIndexPath indexPath: NSIndexPath) ->UITableViewCell {
+    
             let cell =
             tableView.dequeueReusableCellWithIdentifier("CheckListItem")
-                as! UITableViewCell
-            
+              as! UITableViewCell
+
+    
             let label = cell.viewWithTag(1000) as! UILabel
             
-            if indexPath.row % 5 == 0 {
-                label.text = "Walk the dog"
-            } else if indexPath.row % 5 == 1 {
-                label.text = "Brush my Teeth"
-            } else if indexPath.row % 5 == 2 {
-                label.text = "Learn IOS Development"
-            } else if indexPath.row % 5 == 3 {
-                label.text = "soccer practice"
-            } else if indexPath.row % 5 == 4 {
-                label.text = "Eat ice cream"
+            if indexPath.row == 0 {
+                label.text = row0text
+            } else if indexPath.row == 1 {
+                label.text = row1text
+            } else if indexPath.row == 2 {
+                label.text = row2text
+            } else if indexPath.row == 3 {
+                label.text = row3text
+            } else if indexPath.row == 4 {
+                label.text = row4text
             }
-            
-            
+            configureCheckmarkForCell(cell, indexPath: indexPath)
             return cell
             
     }
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+                if let cell = tableView.cellForRowAtIndexPath(indexPath) {
+        var isChecked = false
+        
+        if indexPath.row == 0 {
+        row0checked = !row0checked
+    } else if indexPath.row == 1 {
+     row1checked = !row1checked
+    } else if indexPath.row == 2 {
+     row2checked = !row2checked
+    } else if indexPath.row == 3 {
+     row3checked = !row3checked
+        } else if indexPath.row == 4 {
+     row4checked = !row4checked
+        }
+                configureCheckmarkForCell(cell, indexPath: indexPath)
+                       }
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+                
+           }
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
 }
---
+++ Checklists/ChecklistItem.swift
@@ -0,0 +1,14 @@
+//
+//  ChecklistItem.swift
+//  Checklists
+//
+//  Created by Brad Gray on 5/25/15.
+//  Copyright (c) 2015 Brad Gray. All rights reserved.
+//
+
+import Foundation
+
+class ChecklistItem {
+    var text = ""
+    var checked = false
+}
