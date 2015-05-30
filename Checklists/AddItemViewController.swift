f29e0b03b5c75f57e45a87e94c19027bf95dd515
Bradford Gray <bgray@westmont.edu>
2015-05-29 21:30:23 -0700

    Added a new screen in UI and set up add and back button

---
+++ Checklists/AddItemViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddItemViewController.swift
+//  Checklists
+//
+//  Created by Brad Gray on 5/29/15.
+//  Copyright (c) 2015 Brad Gray. All rights reserved.
+//
+
+import UIKit
+
+class AddItemViewController: UITableViewController {
+
+}
